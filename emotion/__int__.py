b89658aaa90551cb51eb35d68fabddf0459f26d5
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-11 09:20:39 -0700

    chore: add emotion package __init__ with public API exports

---
+++ emotion/__int__.py
@@ -0,0 +1,10 @@
+from emotion.emotion_detector import EmotionDetector, EmotionResult, EmotionLabel
+from emotion.voice_style_mapper import VoiceStyleMapper, VoiceStyle
+
+__all__ = [
+    "EmotionDetector",
+    "EmotionResult",
+    "EmotionLabel",
+    "VoiceStyleMapper",
+    "VoiceStyle",
+]
