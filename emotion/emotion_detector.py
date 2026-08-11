db47cc847fc83c0fbeda1b2dc0a893442f6328db
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-11 08:39:12 -0700

    feat: define EmotionLabel type and VALID_EMOTIONS set for classification

--- emotion/emotion_detector.py
+++ emotion/emotion_detector.py
@@ -6,3 +6,24 @@ from typing import Literal
 from llm.gemini_service import GeminiService
  
 logger = logging.getLogger(__name__)
+
+EmotionLabel = Literal[
+    "happy",
+    "sad",
+    "angry",
+    "confused",
+    "excited",
+    "frustrated",
+    "neutral",
+]
+ 
+VALID_EMOTIONS: set[str] = {
+    "happy",
+    "sad",
+    "angry",
+    "confused",
+    "excited",
+    "frustrated",
+    "neutral",
+}
+ 
